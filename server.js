c5610330058db5bb96f92ec844fefccb67c30ef3
Kevin DA ROCHA <kev.darocha@gmail.com>
2018-07-07 13:51:56 +0200

    Add projects page

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects', {
+    pageTitle: 'My projects'
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
